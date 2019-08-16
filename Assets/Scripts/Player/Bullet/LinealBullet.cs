029d465a0dde396a248a7a625ecc625b36570527
Xebastiano <sebastiangarciacastaneda@gmail.com>
2019-08-16 13:40:30 -0500

    Sprint 2 Tarea 6 Crear scripts de bullets parte 1

--- Assets/Scripts/Player/Bullet/LinealBullet.cs
+++ Assets/Scripts/Player/Bullet/LinealBullet.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LinealBullet : MonoBehaviour
-{
+public class LinealBullet : MonoBehaviour{
+
+    public int speed = 2;
+    public Vector3 direction = Vector3.right;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        transform.Translate(direction * speed * Time.deltaTime);
     }
 }
