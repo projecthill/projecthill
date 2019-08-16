9f0ebde39cded7732471e4181fefe1fbfa55cb50
iTzSlower <iTzSlowerdtc@hotmail.com>
2019-08-15 22:00:46 -0500

    ProgressBar

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public Slider TotalPeople;
+    public float progress;
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        TotalPeople.value = progress;
+        TotalPeople.maxValue = 100f;
+        TotalPeople.minValue = 0f;
+    }
+}
