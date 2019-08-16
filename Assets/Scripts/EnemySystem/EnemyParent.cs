e3a931c5e5999617b3dc14a9f30093938db42602
Debo borrar mi cuenta de git porque la máquina la usan otros profes <o en todo caso no usar global>
2019-08-15 19:31:13 -0500

    Sprint 1 Tareas 8 y 9

---
+++ Assets/Scripts/EnemySystem/EnemyParent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyParent : MonoBehaviour
+{
+    public float hitPoints = 100f; //variable de puntos de vida de cada enemigo
+    public Vector3 direction = Vector3.left; //dirección en la que se mueven los enemigos
+    public float movementSpeed = 10f; //variable de ratio de movimiento de cada enemigo
+    public float damageAmount = 10f; //variable de ratio de daño de cada enemigo le hará a la torre
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
