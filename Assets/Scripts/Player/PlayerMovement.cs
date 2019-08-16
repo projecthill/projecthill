507eb98ca47a41c2c3edd551ffeba8b962cbf613
Xebastiano <sebastiangarciacastaneda@gmail.com>
2019-08-15 20:18:40 -0500

    Sprint 1 Tarea 7 Crear scripts de movimiento del player parte 1

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+
+    public float speed = 0;
+   
+
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        //Movimiento hacia arriba del cañon
+        if (Input.GetKey (KeyCode.W)) {
+            speed = 0.2f;
+            Quaternion temp = transform.rotation;
+            temp.z += speed * Time.deltaTime;
+            transform.rotation = temp;
+            if (transform.rotation.z >= 45) {
+                speed = 0;
+            }
+        }
+        //Movimiento hacia abajo del cañon
+        if (Input.GetKey (KeyCode.S)) {
+            speed = -0.2f;
+            Quaternion temp = transform.rotation;
+            temp.z += speed * Time.deltaTime;
+            transform.rotation = temp;
+        }
+    }
+}
