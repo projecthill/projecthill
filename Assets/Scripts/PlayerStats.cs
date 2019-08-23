afc7fbf577ef3bab147243d10ab311ab3f6bb26e
romarco1 <romarco98@gmail.com>
2019-08-22 20:08:30 -0500

    GameManager y PlayerStats

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public static int Money;
+    public int startMoney = 400;
+
+    public static int Lives;
+    public int startLives = 20;
+
+    public static int Rounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Money = startMoney;
+        Lives = startLives;
+
+        Rounds = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
