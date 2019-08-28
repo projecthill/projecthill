0cb4a160361f7f43572bd0931a4fffb70979794a
Xebastiano <sebastiangarciacastaneda@gmail.com>
2019-08-27 19:30:54 -0500

    Sprint 3 Tarea 3 Terminar scripts de disparo

--- Assets/Scripts/Player/Bullet/BulletGenerator.cs
+++ Assets/Scripts/Player/Bullet/BulletGenerator.cs
@@ -2,30 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletGenerator : MonoBehaviour {
+public class BulletGenerator : MonoBehaviour{
 
     public GameObject BulletPrefab;
-    public float fireRate = 2;
-    public float cooldownTimer = 0;
-    public bool isOnCooldown = false;
-    public Vector2 position { get { return transform.position; }}
+    public float BulletSpace;
+    public float CoolDown;
+    public bool isOnCooldown;
+    public Vector2 position { get { return transform.position; } }
+
+
 
     // Start is called before the first frame update
-    void Start(){
-        
+    void Start (){
+
     }
 
     // Update is called once per frame
-    void Update(){
-        if (Input.GetKeyDown(KeyCode.Space)){
-            GenerateBullet();
-
+    void Update (){
+        if (isOnCooldown == false) {
+            if (Input.GetKeyDown (KeyCode.Space)) {
+                isOnCooldown = true;
+                StartCoroutine (Burst ());
+            }
         }
+        /*if (isOnCooldown == true) {
+            StartCoroutine (CoolingDown ());
+            
+        }*/
     }
 
-    void GenerateBullet() {
-        Debug.Log("PUM!");
-        GameObject bullet = Instantiate(BulletPrefab, position, Quaternion.identity);
-        bullet.GetComponent<LinealBullet>().direction = transform.up;
+
+    void GenerateBullet (){
+        Debug.Log ("PUM!");
+        GameObject bullet = Instantiate (BulletPrefab, position, Quaternion.identity);
+        bullet.GetComponent<LinealBullet> ().direction = transform.up;
+    }
+
+    IEnumerator Burst (){
+        GenerateBullet ();
+        yield return new WaitForSeconds (BulletSpace);
+        GenerateBullet ();
+        yield return new WaitForSeconds (BulletSpace);
+        GenerateBullet ();
+        
+        StartCoroutine (CoolingDown ());
+    }
+    IEnumerator CoolingDown (){
+        yield return new WaitForSeconds (CoolDown);
+        isOnCooldown = false;
     }
 }
