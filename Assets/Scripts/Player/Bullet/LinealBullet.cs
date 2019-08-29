0188b13ac07a6ce4007742840b39b99e58d1e4b6
Xebastiano <sebastiangarciacastaneda@gmail.com>
2019-08-29 15:44:55 -0500

    Sprint 4 tarea 3 terminar Terminar tipos de bala parte 2

--- Assets/Scripts/Player/Bullet/LinealBullet.cs
+++ Assets/Scripts/Player/Bullet/LinealBullet.cs
@@ -9,6 +9,9 @@ public class LinealBullet : MonoBehaviour{
 
     public int damage = 10; //valor de daño de esta bala
 
+    public Vector2 position;
+    public GameObject ExplosionPrefab;
+
     // Start is called before the first frame update
     void Start(){
         
@@ -17,13 +20,26 @@ public class LinealBullet : MonoBehaviour{
     // Update is called once per frame
     void Update(){
         transform.Translate(direction * speed * Time.deltaTime);
+        position = transform.position;
     }
 
     void OnTriggerEnter2D (Collider2D other) {//destruye a un enemigo al tener contacto con él
     
         if (other.CompareTag("Enemy")){
             Destroy(other.gameObject);
+            Destroy(gameObject);
         }
-        Destroy(gameObject);
+
+        if(gameObject.CompareTag("Grenade") && (other.CompareTag("Enemy") || other.CompareTag("Floor"))){
+            Explode();
+            Destroy(gameObject);
+        }
+
     }
+    void Explode(){
+        Debug.Log("KABOOOM!!!!");
+        GameObject explosion = Instantiate(ExplosionPrefab, position, Quaternion.identity);
+        
+    }
+
 }
