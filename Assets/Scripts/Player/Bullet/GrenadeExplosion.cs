cfec9a5749b42c7b7dd4b95d26b9b353f026a698
Xebastiano <sebastiangarciacastaneda@gmail.com>
2019-09-07 15:17:27 -0500

    añadiendo animaciones

--- Assets/Scripts/Player/Bullet/GrenadeExplosion.cs
+++ Assets/Scripts/Player/Bullet/GrenadeExplosion.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GrenadeExplosion : MonoBehaviour
-{
+public class GrenadeExplosion : MonoBehaviour{
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        Destroy (gameObject, 0.2f);
+    void Start(){
+        Destroy (gameObject.GetComponent<CircleCollider2D>(), 0.2f);
+        Destroy (gameObject, 0.5f);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         
     }
+
 }
