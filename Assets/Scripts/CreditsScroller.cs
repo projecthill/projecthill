771d2e40f5552f2a0ffd9daddcc5b76ba3254d1c
Senchi3 <gaboh6464@gmail.com>
2019-09-19 20:15:42 -0500

    Added Credits scene functionality.

---
+++ Assets/Scripts/CreditsScroller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsScroller : MonoBehaviour
+{
+    SceneControl sceneControl;
+
+    bool isScrolling = true;
+
+    void Start ()
+    {
+        sceneControl = FindObjectOfType<SceneControl> ();
+        StartCoroutine (ScrollTime ());
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        if (isScrolling) {
+            transform.position += new Vector3 (0, 3, 0);
+        } else {
+            StartCoroutine (ReleaseTime ());
+        }
+    }
+
+    IEnumerator ScrollTime ()
+    {
+        yield return new WaitForSeconds (21);
+        isScrolling = false;
+    }
+
+    IEnumerator ReleaseTime ()
+    {
+        new WaitForSeconds (0.1f);
+        sceneControl.LoadScene (0);
+        yield return null;
+    }
+}
