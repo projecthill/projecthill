cf9c6dca1f6894c745fc465288b2ce5bc03c2f73
Rayxor <dheredia0988@gmail.com$ git config --global user.email dheredia0988@gmail.com>
2019-09-24 07:55:28 -0500

    Se subió la música a la carpeta Resources

---
+++ Assets/Scripts/SpawnSystem/SpawnTimer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SpawnTimer : MonoBehaviour
+{
+    float uiTimer;
+    public Text textBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateUIValues()
+    {
+
+        if (uiTimer <= 0)
+        {
+            uiTimer = 0;
+            textBox.text = "Waiting for current wave...";
+        }
+        else
+        {
+            uiTimer -= Time.deltaTime;
+            textBox.text = "Next wave starts in: " + Mathf.Round(uiTimer).ToString();
+        }
+    }
+
+}
--- Assets/Scripts/SpawnSystem/Spawner.cs
+++ Assets/Scripts/SpawnSystem/Spawner.cs
@@ -17,8 +17,7 @@ public class Spawner : MonoBehaviour
     public float currentDelta;
     public float variantTimer;
 
-    float uiTimer;
-    public Text textBox;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -40,7 +39,7 @@ public class Spawner : MonoBehaviour
     {
         currentTimer += Time.deltaTime; //currentTimer aumenta en Time.deltaTime
         ResetTimer(SpawnEntity);
-        UpdateUIValues();
+        
     }
 
     void ResetTimer(Action action) //resetea el timer a 0 al empezar cada wave
@@ -62,22 +61,10 @@ public class Spawner : MonoBehaviour
         currentDelta = wave.timeDelta;
         variantTimer = wave.timer;
 
-        uiTimer = spawnDelay;
+        
     }
 
-    void UpdateUIValues ()
-    {
-        
-        if (uiTimer <= 0)
-        {
-            uiTimer = 0;
-            textBox.text = "Waiting for current wave...";
-        } else
-        {
-            uiTimer -= Time.deltaTime;
-            textBox.text = "Next wave starts in: " + Mathf.Round(uiTimer).ToString();
-        }
-    }
+   
 
     void ParseData (string textData)
     {
