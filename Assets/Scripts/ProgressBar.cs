2ef9b20f1ed8d20939af5ca8f720d9ac78b1dd4f
dheredia0988 <dheredia0988@gmail.com>
2019-09-24 20:48:12 -0500

    T menos 5 y contando

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -8,23 +8,31 @@ public class ProgressBar : MonoBehaviour
     public Slider totalPeople;
     public float timeDelay;
     public float progress;
+    public GameObject WIN;
 
     // Start is called before the first frame update
     void Start() {
         progress = timeDelay;
+        
     }
 
     // Update is called once per frame
     void Update() {
-
         progress -= Time.deltaTime;
-
-        if(progress <= 0) {
-            progress = timeDelay;
-        }
 
         totalPeople.value = progress;
         totalPeople.maxValue = 100f;
         totalPeople.minValue = 0f;
+
+        OpenWIN();
+    }
+
+    public void OpenWIN(){
+
+        if (progress <= 0){
+            WIN.SetActive(true);
+            Time.timeScale = 0;
+
+        }
     }
 }
